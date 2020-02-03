6b48116d8923a8a8ef1ff1833c16f006fb8d6c89
Bendik <knaben@OBOS.NO>
2020-02-03 15:35:47 +0100

    black formatting

--- Bot.py
+++ Bot.py
@@ -1,10 +1,13 @@
-from selenium import Webdriver
+from selenium import webdriver
 
 
-class Bot():
+class Bot:
+    ID = 0
 
     def __init__(self):
-
+        Bot.ID += 1
+        self.ID = Bot.ID
+        self.driver = self.load_driver()
 
     def load_driver(self, headless=True):
         """Opens a webdriver instance with chromedriver
@@ -18,10 +21,18 @@ class Bot():
             options.add_argument("headless")
         options.add_argument("--window-size=1920,1080")
         options.add_argument("--log-level=3")
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
         options.add_argument("--silent")
         options.add_argument("user-agent=Obosbot")
-        driver = webdriver.Chrome(
-            executable_path=r"chromedriver.exe",
-            options=options,
-        )
+        driver = webdriver.Chrome(executable_path=r"chromedriver.exe", options=options)
+        # print(f"driver {self.ID} loaded")
         return driver
+
+    def get_html(self, url):
+        self.driver.get(url)
+        html = self.driver.page_source
+        return (url, html)
+
+    def quit(self):
+        # print(f"quiting driver {self.ID}")
+        self.driver.quit()
