497110a867b3b851725f57e684cb1348ff76b714
Bendik <knaben@OBOS.NO>
2020-02-04 14:32:20 +0100

    add actual_url change name getters and setters

--- Page.py
+++ Page.py
@@ -3,9 +3,16 @@ from urllib.parse import urljoin
 
 
 class Page:
-    def __init__(self, html: str, url: str):
+    def __init__(self, url: str, html: str, actual_url: str):
         self.html = html
         self.url = url
+        self.actual_url = actual_url
+        self.redirected = self.url not in actual_url
+
+    def __str__(self):
+        representation = f"Url: {self.url}\nActual_url: {self.actual_url}\nIs redirected: {self.redirected}\nMeta_Description: {self.description}\nMeta_title: {self.meta_title}"
+        return representation
+        
 
     def parse_html(self):
         soup = BS(self.html, "lxml")
@@ -15,30 +22,30 @@ class Page:
         soup = BS(self.html, "lxml")
         self.soup = soup
 
-    def get_meta(self):
+    def set_meta(self):
         metas = self.soup.findAll("meta")
         for i in metas:
-            # print(i)
-            # print(f'{i.get("name")} {i.get("content")}')
             if i.get("name") == "description":
                 self.description = i.get("content")
             if i.get("name") == "title":
                 self.meta_title = i.get("content")
 
-    def get_links(self, html):
+    def set_links(self, html):
         self.links = []
         soup = BS(html, "lxml")
         for link in soup.findAll("a"):
             if link.has_attr("href"):
                 self.links.append(urljoin("https://www.obos.no", link["href"]))
 
-    def return_links(self):
+    def get_links(self):
         if not self.links:
-            self.get_links(self.html)
+            self.set_links(self.html)
         return self.links
 
-    def get_images(self):
+    def set_images(self):
         self.images = []
         imgs = self.soup.findAll("img")
         for i in imgs:
-            self.images.append((urljoin(self.url, i.get("src")), i.get("alt")))
+            self.images.append((urljoin(self.url, i.get("src")), i.get("alt","")))
+
+    
