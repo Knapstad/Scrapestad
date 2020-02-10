d26cef0846253a6665ed80ab53ce60a44a6279b0
Bendik <knaben@OBOS.NO>
2020-02-10 12:25:15 +0100

    added page and tableworkers

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from Page import Page
 from Bot import Bot
 from threading import Thread
@@ -6,34 +9,52 @@ from queue import Queue
 
 
 
-def worker(queue):
-    
+def page_worker(work_queue, worked_queue, behandleslist, erbehandlet):
     try:
         bot = Bot()
         while True:
-            url = queue.get()
-            print(url)
+            url = work_queue.get()
             html = bot.get_html(url)
             page = Page(html)
+            worked_queue.put(page)
             erbehandlet.append(page)
-            for i in page.links:
-                if i not in behandleslist and"obos.no" in i:
-                    queue.put(i)
-                    behandleslist.append(i)
-            # a.task_done()
+            for url in page.links:
+                if url not in behandleslist and "www.obos.no" in url and "#main" not in url and "page=" not in url:
+                    work_queue.put(url)
+                    behandleslist.append(url)
     finally:
-
         bot.quit()
 
-def run_page_workers(first: str, num_workers: int):
+def table_worker(queue, table):
+    while True:
+        print("tableworker")
+        page=queue.get()
+        print(f"tableworker {page.title}")
+        currentRowCount = table.rowCount()
+        table.setRowCount(currentRowCount + 1)
+        table.setItem(currentRowCount, 0, QTableWidgetItem(f"{page.title}"))
+        table.setItem(currentRowCount, 1, QTableWidgetItem(f"{page.url}"))
+        table.setItem(currentRowCount, 2, QTableWidgetItem(f"{page.actual_url}"))
+        table.setItem(currentRowCount, 3, QTableWidgetItem(f"{page.redirected}"))
+        table.setItem(currentRowCount, 4, QTableWidgetItem(f"{page.description}"))
+        table.setItem(currentRowCount, 5, QTableWidgetItem(f"{len(page.images)}"))
+        table.setItem(currentRowCount, 6, QTableWidgetItem(f"{page.links}"))
+
+
+
+def run_page_workers(first: str, num_workers: int, table):
+    table=table
     skalbehandles = Queue()
+    erbehandletqueue = Queue()
     behandleslist = []
     erbehandlet = []
     try:
         skalbehandles.put(first)
         for i in range(4):
-            p = Thread(target=worker, args=(skalbehandles,))
+            p = Thread(target=page_worker, args=(skalbehandles,erbehandletqueue,behandleslist,erbehandlet))
             p.start()
-    
+        tabwork = Thread(target=table_worker, args=(erbehandletqueue,table))
+        tabwork.start()
     except Exception as e:
         print(e)
+
