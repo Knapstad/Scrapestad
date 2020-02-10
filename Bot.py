81d7bb12cc92c8130032b4ede015247bc60c82e9
Bendik <knaben@OBOS.NO>
2020-02-10 09:39:07 +0100

    change indentlevel

--- Bot.py
+++ Bot.py
@@ -17,23 +17,23 @@ class Bot:
 
 
     def load_driver(self, headless=True):
-        """Opens a webdriver instance with chromedriver
-                
-        Returns:
-        Webdriver  -- The webdriver instance
-        """
+            """Opens a webdriver instance with chromedriver
+                    
+            Returns:
+            Webdriver  -- The webdriver instance
+            """
 
-        options = webdriver.ChromeOptions()
-        if headless:
-            options.add_argument("headless")
-        options.add_argument("--window-size=1920,1080")
-        options.add_argument("--log-level=3")
-        options.add_experimental_option("excludeSwitches", ["enable-logging"])
-        options.add_argument("--silent")
-        options.add_argument("user-agent=Obosbot")
-        driver = webdriver.Chrome(executable_path=r"chromedriver.exe", options=options)
-        # print(f"driver {self.ID} loaded")
-        return driver
+            options = webdriver.ChromeOptions()
+            if headless:
+                options.add_argument("headless")
+            options.add_argument("--window-size=1920,1080")
+            options.add_argument("--log-level=3")
+            options.add_experimental_option("excludeSwitches", ["enable-logging"])
+            options.add_argument("--silent")
+            options.add_argument("user-agent=Obosbot")
+            driver = webdriver.Chrome(executable_path=r"chromedriver.exe", options=options)
+            # print(f"driver {self.ID} loaded")
+            return driver
 
     def get_html(self, url) -> tuple:
         self.url = urlparse(url,scheme="https")
