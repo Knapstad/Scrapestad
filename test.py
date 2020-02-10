084411018137aa0f2add870b396dedee96eb9cbf
Bendik <knaben@OBOS.NO>
2020-02-10 10:11:38 +0100

    multithreading test

--- test.py
+++ test.py
@@ -1,12 +1,43 @@
 from Page import Page
 from Bot import Bot
+from threading import Thread
+from queue import Queue
+# import time
 
 
-bot = Bot()
-html = bot.get_html("https://www.vg.no")
-page = Page(html[0], html[1], html[2])
-page.make_soup()
-page.set_meta()
-page.set_images()
 
-print(page)
+def worker(queue):
+    
+    try:
+        bot = Bot()
+        while True:
+            url = queue.get()
+            print(url)
+            html = bot.get_html(url)
+            page = Page(html)
+            erbehandlet.append(page)
+            for i in page.links:
+                if i not in behandleslist and"obos.no" in i:
+                    queue.put(i)
+                    behandleslist.append(i)
+                if len(erbehandlet) >=10:
+                    
+                    return 
+           
+            # a.task_done()
+    finally:
+
+        bot.quit()
+
+def run_page_workers(first: str, num_workers: int):
+    skalbehandles = Queue()
+    behandleslist = []
+    erbehandlet = []
+    try:
+        skalbehandles.put(first)
+        for i in range(4):
+            p = Thread(target=worker, args=(skalbehandles,))
+            p.start()
+    
+    except Exception as e:
+        print(e)
