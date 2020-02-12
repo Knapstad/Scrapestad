cc224aae448ee2ba86edf08afd4b5da15b32e890
Bendik <knaben@OBOS.NO>
2020-02-12 14:44:22 +0100

    add test

--- test.py
+++ test.py
@@ -3,25 +3,35 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from Page import Page
 from Bot import Bot
+from multiprocessing import Process, Queue, Manager
 from threading import Thread
-from queue import Queue
+from queue import Queue as Koo
 # import time
 
 
 
 def page_worker(work_queue, worked_queue, behandleslist, erbehandlet):
     try:
+        print(f"page worker start")
         bot = Bot()
         while True:
             url = work_queue.get()
+            if url in erbehandlet:
+                continue
             html = bot.get_html(url)
             page = Page(html)
+            page.soup=None
+            page.html=None
             worked_queue.put(page)
-            erbehandlet.append(page)
+            erbehandlet.append(page.url)
+            # erbehandlet.append(page.actual_url)
+            negate = ["page=", "-jpeg", "pid=", "/calendar/createevent", "#", "-pdf"]
             for url in page.links:
-                if url not in behandleslist and "www.obos.no" in url and "#main" not in url and "page=" not in url:
+                if url not in behandleslist and "www.obos.no" in url and not any(word in url for word in negate):
                     work_queue.put(url)
                     behandleslist.append(url)
+            # print(f"\rIn work queue: {work_queue.qsize()}, In worked queue {worked_queue.qsize()}, er behandlet: {len(erbehandlet)}", end="")
+
     finally:
         bot.quit()
 
@@ -37,22 +47,30 @@ def table_worker(queue, table):
         table.setItem(currentRowCount, 4, QTableWidgetItem(f"{page.description}"))
         table.setItem(currentRowCount, 5, QTableWidgetItem(f"{len(page.images)}"))
         table.setItem(currentRowCount, 6, QTableWidgetItem(f"{page.links}"))
+        
 
 
 
 def run_page_workers(first: str, num_workers: int, table):
     table=table
+    manager= Manager()
     skalbehandles = Queue()
     erbehandletqueue = Queue()
-    behandleslist = []
-    erbehandlet = []
+    behandleslist = manager.list()
+    erbehandlet = manager.list()
+    workers=[]
     try:
         skalbehandles.put(first)
-        for i in range(4):
-            p = Thread(target=page_worker, args=(skalbehandles,erbehandletqueue,behandleslist,erbehandlet))
-            p.start()
+        for i in range(num_workers):
+            print("starting process " + str(i))
+            p = Process(target=page_worker, args=(skalbehandles,erbehandletqueue,behandleslist,erbehandlet))
+            workers.append(p)
+        for i in workers:
+            i.start()
         tabwork = Thread(target=table_worker, args=(erbehandletqueue,table))
         tabwork.start()
+        for i in workers:
+            i.join()
     except Exception as e:
         print(e)
 
