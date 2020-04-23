eda0bb2d135435e401fc6cb319bd75dceecdf65d
Bendik Knapstad <knaben@OBOS.NO>
2020-04-23 10:32:48 +0200

    add queue support and error handling

--- async_driver.py
+++ async_driver.py
@@ -3,15 +3,20 @@ import asyncio
 from arsenic import get_session, browsers, services
 import random
 
-async def get(url):
+async def get(urls):
     service = services.Chromedriver(binary="C://Users/knaben/Documents/Scripts/Python/scraper/chromedriver.exe")
     browser = browsers.Chrome(chromeOptions={'args': ['--headless', '--disable-gpu', '--silent']})
-    async with get_session(service, browser) as session:
-        await session.get(url)
-        await asyncio.sleep(random.randint(1,2))
-        title = await session.get_element("title")
-        title = await title.get_text()
-        print(title)
-        print("im done")
+    while not urls.empty():
+        try:
+            url = urls.get_nowait()
+            async with get_session(service, browser) as session:
+                await session.get(url[0])
+        
+                title = await session.get_element("title")
+                title = await title.get_text()
+            urls.task_done()
+        except Exception as e:
+            print(e)
+            break  
 
-asyncio.run(get("https://www.obos.no"))
+
