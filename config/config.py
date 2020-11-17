dc154d84b8dd6541b25c761c2ac2e009d3b19e0d
Bendik Knapstad <knaben@OBOS.NO>
2020-11-17 13:55:34 +0100

    add pathlib

--- config/config.py
+++ config/config.py
@@ -1,19 +1,20 @@
 import json
 import os
+from pathlib import Path
 
 
-file_prefix = os.path.dirname(__file__)
+file_prefix = Path(os.path.dirname(__file__))
 
-with open(f"{file_prefix}\config.json") as file:
+with open(file_prefix/"config.json") as file:
     config = json.load(file)
 
 
 def set_config(key, value):
     config[key] = value
-    with open("config/config.json", "w") as file:
+    with open(file_prefix/"config.json", "w") as file:
         json.dump(config, file)
 
 def clear_config():
     config = {}
-    with open("config/config.json", "w") as file:
+    with open(file_prefix/"config.json", "w") as file:
         json.dump(config, file)
