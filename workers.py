83ea469f62e1f6f67b3fe865d30293c7358f24e6
Bendik Knapstad <b.knapstad@gmail.com>
2020-12-23 20:01:50 +0100

    trnanselate

--- workers.py
+++ workers.py
@@ -32,36 +32,38 @@ class Worker(QRunnable):
         try:
             self.fn(*self.args, **self.kwargs)
         except Exception as e:
-            print(e)
+            # print(e)
 
 
 
 def page_worker(work_queue: Queue, worked_queue: Queue, tobeprocessed: dict, isprocessed: dict, run: Value):
     empty=0
     try:
-        # print(f"Starting worker: {workerid}")
+        # print(f"Starting worker")
         bot = Bot()
         while not work_queue.empty():
             if not run.value:
                 continue
             else:
-                # print(f"worker: {workerid} queue not epmpty")
+                # print(f"worker: queue not epmpty")
                 
                 # if len(erbehandlet) > 30:
                 #     break
                 try:
-                    # print(f" worker: {workerid} getting from queue")
+                    # print(f" worker:  getting from queue")
                     url = work_queue.get(timeout=1)
-                    # print(f" worker: {workerid} {url}") 
+                    # print(f" worker:  {url}") 
                 except Empty:
-                    # print(f" worker: {workerid} in except")
+                    # print(f" worker:  in except Empty")
                     empty+=1
                     # print(f" worker: {workerid} {empty}")
                     # time.sleep(2)
                     if empty >= 3:
+                        # print("empty > 3")
                         return
                     continue
                 if url in isprocessed:
+                    # print("{url} already processed")
                     continue
                 html = bot.get_html(url)
                 page = Page(html)
@@ -73,16 +75,19 @@ def page_worker(work_queue: Queue, worked_queue: Queue, tobeprocessed: dict, isp
                 # erbehandlet.append(page.actual_url)
                 negate = config["negate"]
                 for url in page.links:
-                    if url not in tobeprocessed and config["domain"] in url and not any(word in url for word in negate):
+                    # print(f"{url} in tobeprocessed: {url in tobeprocessed}")
+                    # print(f"{config['domain']} in url: {config['domain'] in url}")
+                    # print(f"{url} in negate: {any(word in url for word in negate)}")
+                    if url not in tobeprocessed and config['domain'] in url and not any(word in url for word in negate):
                         work_queue.put(url)
                         tobeprocessed[url]=1
-                # print(f"\rIn work queue: {work_queue.qsize()}, In worked queue {worked_queue.qsize()}, er behandlet: {len(erbehandlet)}", end="")
+                print(f"\rIn work queue: {work_queue.qsize()}, In worked queue {worked_queue.qsize()}, er behandlet: {len(erbehandlet)}", end="")
 
 
     finally:
         bot.quit()
         worked_queue.put("done")
-        # print(f"Pageworker {workerid} done")
+        # print(f"Pageworker  done")
 
 def threader(work_queue, worked_queue, tobeprocessed, isprocessed, workerid, process):
     workers=[]
@@ -110,7 +115,7 @@ def table_worker(queue: Queue, table: QTableView, num_workers: int, antall: QLab
             if page == "done":
                 done+=1
                 if done >= num_workers:
-                    # print("done")
+                    # print("Tabel worker done")
                     return
                 continue
 
@@ -126,7 +131,7 @@ def table_worker(queue: Queue, table: QTableView, num_workers: int, antall: QLab
             table.setItem(currentRowCount, 7, QTableWidgetItem(f"{len(page.images_blank_alt)}"))
             table.setItem(currentRowCount, 8, QTableWidgetItem(f"{len(page.images_missing_alt)}"))
             table.setItem(currentRowCount, 9, QTableWidgetItem(f"{page.links}"))
-            antall.setText(f"Er behandlet: {len(isprocessed)}\nSkal behandles: {len(tobeprocessed)}\nI tabell: {currentRowCount}")
+            antall.setText(f"Is processed: {len(isprocessed)}\nTo be processed: {len(tobeprocessed)}\nIn table: {currentRowCount}")
 
         except Empty:
                 continue
@@ -156,6 +161,6 @@ def run_page_workers(first: str, num_workers: int, table: QTabWidget, antall: in
         for i in workers:
             # print(f"joining {i}")
             i.join()
-        antall.setText(f"Antall urler {table.rowCount()}")
+        antall.setText(f"Number of urls: {table.rowCount()}")
     except Exception as e:
         print(e)
